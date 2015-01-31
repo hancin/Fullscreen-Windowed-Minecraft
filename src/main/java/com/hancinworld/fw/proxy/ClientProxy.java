e5902b2d61362e1493511e859ba0701c91aa0757
hancin <hancin@hancinworld.com>
2015-01-31 10:30:15 -0500

    After testing, do the change in preInit
    It allows the game to recover faster as there are minimal resources
    loaded at that point

--- src/main/java/com/hancinworld/fw/proxy/ClientProxy.java
+++ src/main/java/com/hancinworld/fw/proxy/ClientProxy.java
@@ -12,7 +12,7 @@ public class ClientProxy extends CommonProxy {
     public void toggleFullScreen(boolean state) {
         System.setProperty("org.lwjgl.opengl.Window.undecorated", state?"true":"false");
         try {
-            Display.setResizable(false);
+            Display.setResizable(!state);
             Display.setFullscreen(false);
             Display.setDisplayMode(new DisplayMode( Display.getDesktopDisplayMode().getWidth(),  Display.getDesktopDisplayMode().getHeight()));
         } catch (LWJGLException e) {
