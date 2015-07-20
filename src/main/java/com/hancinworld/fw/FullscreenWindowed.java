fbf7b93b207a8b0ae767988028abac33208520df
hancin <hancin@hancinworld.com>
2015-07-20 18:33:46 -0400

    Add license header to all files

--- src/main/java/com/hancinworld/fw/FullscreenWindowed.java
+++ src/main/java/com/hancinworld/fw/FullscreenWindowed.java
@@ -1,3 +1,25 @@
+//Copyright (c) 2015, David Larochelle-Pratte
+//All rights reserved.
+//
+//        Redistribution and use in source and binary forms, with or without
+//        modification, are permitted provided that the following conditions are met:
+//
+//        1. Redistributions of source code must retain the above copyright notice, this
+//        list of conditions and the following disclaimer.
+//        2. Redistributions in binary form must reproduce the above copyright notice,
+//        this list of conditions and the following disclaimer in the documentation
+//        and/or other materials provided with the distribution.
+//
+//        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//        ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//        ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package com.hancinworld.fw;
 
 import com.hancinworld.fw.handler.ConfigurationHandler;
@@ -8,8 +30,6 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.*;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
 
 @Mod(modid = Reference.MOD_ID, name=Reference.MOD_NAME,version=Reference.VERSION,guiFactory = Reference.GUI_FACTORY_CLASS)
 public class FullscreenWindowed {
--- src/main/java/com/hancinworld/fw/proxy/ClientProxy.java
+++ src/main/java/com/hancinworld/fw/proxy/ClientProxy.java
@@ -1,3 +1,25 @@
+//Copyright (c) 2015, David Larochelle-Pratte
+//All rights reserved.
+//
+//        Redistribution and use in source and binary forms, with or without
+//        modification, are permitted provided that the following conditions are met:
+//
+//        1. Redistributions of source code must retain the above copyright notice, this
+//        list of conditions and the following disclaimer.
+//        2. Redistributions in binary form must reproduce the above copyright notice,
+//        this list of conditions and the following disclaimer in the documentation
+//        and/or other materials provided with the distribution.
+//
+//        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//        ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//        ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package com.hancinworld.fw.proxy;
 
 import com.hancinworld.fw.handler.ConfigurationHandler;
@@ -12,9 +34,6 @@ import org.lwjgl.opengl.DisplayMode;
 
 import java.lang.reflect.Method;
 
-/**
- * Created by David on 2015-01-31.
- */
 public class ClientProxy extends CommonProxy {
 
     private int _lastRegisteredWidth = 0;
@@ -28,13 +47,12 @@ public class ClientProxy extends CommonProxy {
     public void registerKeyBindings()
     {
 
+        /* FIXME: Overrides the minecraft hotkey for fullscreen, as there are no hooks */
         if(ConfigurationHandler.overrideF11Behavior)
         {
             Minecraft mc = Minecraft.getMinecraft();
-
             fullscreenKeyBinding = mc.gameSettings.field_152395_am;
-
-           mc.gameSettings.field_152395_am = ignoreKeyBinding;
+            mc.gameSettings.field_152395_am = ignoreKeyBinding;
         }
     }
 
