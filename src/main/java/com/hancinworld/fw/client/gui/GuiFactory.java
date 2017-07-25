bf1b1a2d0dee480be5d41cd53a6dc0614238c1ab
hancin <hancin@hancinworld.com>
2017-07-25 17:39:45 -0400

    This branch is a backport for 1.10 so the changes must be rollback in that one.

--- src/main/java/com/hancinworld/fw/client/gui/GuiFactory.java
+++ src/main/java/com/hancinworld/fw/client/gui/GuiFactory.java
@@ -38,20 +38,17 @@ public class GuiFactory implements IModGuiFactory {
     }
 
     @Override
-    public boolean hasConfigGui() {
-        return true;
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return ModGuiConfig.class;
     }
-
-    @Override
-    public GuiScreen createConfigGui(GuiScreen parentScreen) {
-        return new ModGuiConfig(parentScreen);
-    }
-
 
     @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
         return null;
     }
 
-
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+        return null;
+    }
 }
